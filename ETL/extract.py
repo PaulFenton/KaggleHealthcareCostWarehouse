45ab9885a7d63ba882a37336f6e75e54be89d4f0
PaulFenton <paulfenton@dal.ca>
2018-05-31 20:23:18 -0500

    finished python stuff for now

--- ETL/extract.py
+++ ETL/extract.py
@@ -1,2 +1,7 @@
+import pandas as pd
 def extract():
-    return
+    df = pd.read_csv("../source_data/Hospital_Inpatient_Discharges__SPARCS_De-Identified___2015.csv", low_memory=False, error_bad_lines=False)
+
+    # todo: do something here
+
+    return df
