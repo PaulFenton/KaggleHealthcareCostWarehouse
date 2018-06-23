ef57787e56f407d4b6a7144dfbea8102a12dd52f
PaulFenton <paulfenton@dal.ca>
2018-06-23 13:26:02 -0500

    added initial extract code

--- ETL/extract.py
+++ ETL/extract.py
@@ -1,7 +1,11 @@
 import pandas as pd
 def extract():
-    df = pd.read_csv("../source_data/Hospital_Inpatient_Discharges__SPARCS_De-Identified___2015.csv", low_memory=False, error_bad_lines=False)
 
-    # todo: do something here
+    # 2.4 M rows in csv need to be read into a dataframe in chunks
+    list = []
+    for chunk in pd.read_csv("../source_data/Hospital_Inpatient_Discharges__SPARCS_De-Identified___2015.csv", chunksize=20000, low_memory=False, error_bad_lines=False):
+        list.append(chunk)
+    df = pd.concat(list, axis=0)
+    del list
 
     return df
